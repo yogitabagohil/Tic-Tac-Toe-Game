ed2c9a0a6d7689841b962e94df8f7cc1ea6aa3a9
yogitabagohil <yogitabagohil2022@gmail.com>
2024-06-12 12:56:00 +0300

    first commit

---
+++ src/Info.js
@@ -0,0 +1,13 @@
+const Info = () =>{
+    return(
+        <div className="info">
+            <div className="player">
+            Player 1:
+            </div>
+            <div className="player">
+            Player 2:
+            </div>
+        </div>
+    )
+}
+export default Info;
