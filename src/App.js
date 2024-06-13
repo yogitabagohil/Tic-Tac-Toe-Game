0f24104931d08508e53b7c2775ae885061abbd64
yogitabagohil <yogitabagohil2022@gmail.com>
2024-06-13 11:33:00 +0300

    Add reset button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Board from './Board';
 import Info from "./Info";
+import { useState } from "react";
 
 function App() {
+  const [winner, setWinner] = useState("");
   return (
     <div className="App">
+      <div>
+      <div className="winner-text">{winner}</div>
+        <button>
+          Reset Board
+        </button>
+      </div>
       <Board />
-     <Info />
+      <Info />
     </div>
   );
 }
--- src/Board.js
+++ src/Board.js
@@ -3,16 +3,15 @@ const Board = () =>{
     return(
         <div className="board">
             <div className="input input-1"></div>
-            <div className="input input-1"></div>
+            <div className="input input-2"></div>
             <div className="input input-3"></div>
-            <div className="input"></div>
-            <div className="input"></div>
-            <div className="input"></div>
-            <div className="input"></div>
-            <div className="input"></div>
-            <div className="input"></div>
-            <div className="input"></div>
-
+            <div className="input input-4"></div>
+            <div className="input input-5"></div>
+            <div className="input input-6"></div>
+            <div className="input input-7"></div>
+            <div className="input input-8"></div>
+            <div className="input "></div>
+           
         </div>
     )
 
